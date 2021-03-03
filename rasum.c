56e4a4f066f7f6f08e1de9fc72fbfc5f9bc80688
Neil Munday <musclesmunday@hotmail.com>
2021-03-03 22:11:52 +0000

    Changed to MIT license

--- rasum.c
+++ rasum.c
@@ -1,30 +1,31 @@
 /*
-    This file is part of the Pi Entertainment System (PES).
+		MIT License
 
-    PES provides an interactive GUI for games console emulators
-    and is designed to work on the Raspberry Pi.
+		Copyright (c) 2021 Neil Munday
 
-    Copyright (C) 2021 Neil Munday (neil@mundayweb.com)
+		Permission is hereby granted, free of charge, to any person obtaining a copy
+		of this software and associated documentation files (the "Software"), to deal
+		in the Software without restriction, including without limitation the rights
+		to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+		copies of the Software, and to permit persons to whom the Software is
+		furnished to do so, subject to the following conditions:
 
-    PES is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+		The above copyright notice and this permission notice shall be included in all
+		copies or substantial portions of the Software.
 
-    PES is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with PES.  If not, see <http://www.gnu.org/licenses/>.
+		THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+		IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+		FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+		AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+		LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+		OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+		SOFTWARE.
 
     Description:
 
     This program takes a ROM and calculates its RetroAchivements.org hash value.
 
-    Use the -t option to specify the ROM type (nes, snes, genesis or generic).
-    If not specified, then "generic" is assumed.
+    Use the -i option to specify the console ID as defined by RetroAchivements.org
 
 		Acknowledgements:
 
